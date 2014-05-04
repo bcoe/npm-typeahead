5690ce19f4aff3c0503e4e6c4de58d5cf97224cc
bcoe <bencoe@gmail.com>
2014-05-03 21:00:12 -0700

    basic directory structure.

---
+++ test/search.js
@@ -0,0 +1,14 @@
+var assert = require('assert'),
+  Server = require('../lib').Server;
+
+describe("Server", function() {
+
+  describe("start", functino() {
+
+    it('should start the restify server', function() {
+
+    });
+
+  });
+
+});
