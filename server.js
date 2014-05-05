cdfa0f094af56bf945dfbcdb7afc20bf5b272dd4
bcoe <bencoe@gmail.com>
2014-05-04 17:21:08 -0700

    added package lookup endpoint.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-var restify = require('restify'), // require the restify library.
-  server = restify.createServer(); // create an HTTP server.
+var restify = require('restify'),
+  server = restify.createServer(),
+  search = new (require('./lib').Search);
 
+server.use(restify.queryParser());
+
+// lookup packages by their name.
+server.get('/search', function(req, res, cb) {
+  search.search(req.params.q, function(err, results) {
+    res.send(results);
+    cb();
+  });
+});
+
+// serve static JavaScript and CSS.
 server.get(/\/js|css\/?.*/, restify.serveStatic({
   directory: './assets'
 }));
 
+// serve the static index page.
 server.get(/\/?/, restify.serveStatic({
   directory: './assets',
   default: 'index.html'
 }));
 
-server.listen(process.env.PORT || 5000, function () { // bind server to port 5000.
+// bind server to on port 5000, or the port provided.
+server.listen(process.env.PORT || 5000, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
